db58973cae4a98b6a69c9ffc989c96ce1d627473
Allen Peng <boonya@sina.com>
2017-04-27 16:09:58 +0800

    Add more test methods.

--- src/test/java/com/boonya/thread/BThreadPoolTest.java
+++ src/test/java/com/boonya/thread/BThreadPoolTest.java
@@ -3,7 +3,9 @@ package com.boonya.thread;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+
 import org.junit.Test;
 
 /**
@@ -30,6 +32,31 @@ public class BThreadPoolTest {
 	public void testFixedThreadPool(){
 		// 线程内空闲保持poolSize个线程
 		ExecutorService executorService=Executors.newFixedThreadPool(poolSize);
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.shutdown();
+	}
+	/**
+	 * 线程保持固定个数的线程存活数量
+	 * 
+	 * @MethodName: testFixedThreadPoolThreadFactory 
+	 * @Description: 
+	 * @throws
+	 */
+	@Test
+	public void testFixedThreadPoolThreadFactory(){
+		// 线程内空闲保持poolSize个线程
+		ThreadFactory threadFactory=Executors.defaultThreadFactory();
+		ExecutorService executorService=Executors.newFixedThreadPool(poolSize,threadFactory);
 		
 		executorService.execute(new Task());
 		
@@ -70,6 +97,32 @@ public class BThreadPoolTest {
 	}
 	
 	/**
+	 * 按需缓存线程池
+	 * 
+	 * @MethodName: testCachedThreadPoolThreadFactory 
+	 * @Description: 
+	 * @throws
+	 */
+	@Test
+	public void testCachedThreadPoolThreadFactory(){
+		// 有任务时才创建新线程，空闲线程保留60秒
+		ThreadFactory threadFactory=Executors.defaultThreadFactory();
+		ExecutorService executorService=Executors.newCachedThreadPool(threadFactory);
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.shutdown();
+	}
+	
+	/**
 	 * 单个线程依次执行任务线程池
 	 * 
 	 * @MethodName: testSingleThreadExecutor 
@@ -80,6 +133,32 @@ public class BThreadPoolTest {
 	public void testSingleThreadExecutor(){
 		// 单个线程依次执行任务
 		ExecutorService executorService=Executors.newSingleThreadExecutor();
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.shutdown();
+	}
+	
+	/**
+	 * 单个线程依次执行任务线程池
+	 * 
+	 * @MethodName: testSingleThreadExecutorWithThradFactory 
+	 * @Description: 
+	 * @throws
+	 */
+	@Test
+	public void testSingleThreadExecutorWithThradFactory(){
+		// 单个线程依次执行任务
+		ThreadFactory threadFactory=Executors.defaultThreadFactory();
+		ExecutorService executorService=Executors.newSingleThreadExecutor(threadFactory);
 		
 		executorService.execute(new Task());
 		
@@ -122,6 +201,34 @@ public class BThreadPoolTest {
 	}
 	
 	/**
+	 * @throws InterruptedException 
+	 * 单个线程依次按时间计划执行任务线程池
+	 * 
+	 * @MethodName: testSingleThreadScheduledExecutorWithThradFactory 
+	 * @Description: 
+	 * @throws
+	 */
+	@Test
+	public void testSingleThreadScheduledExecutorWithThradFactory() throws InterruptedException{
+		// 单个线程依次按计划执行任务
+		ThreadFactory threadFactory=Executors.defaultThreadFactory();
+		ScheduledExecutorService executorService=Executors.newSingleThreadScheduledExecutor(threadFactory);
+		executorService.awaitTermination(300, TimeUnit.MILLISECONDS); 
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.shutdown();
+	}
+	
+	/**
 	 * 设置计划任务调用的线程池
 	 * 
 	 * @throws InterruptedException 
@@ -135,6 +242,37 @@ public class BThreadPoolTest {
 		// 线程内创建至少poolSize个线程，可以根据业务处理设置处理时间 
 		//ScheduledExecutorService继承自ExecutorService
 		ScheduledExecutorService  executorService=Executors.newScheduledThreadPool(poolSize);
+		executorService.awaitTermination(3000, TimeUnit.MILLISECONDS);
+		//ExecutorService executorService=Executors.newScheduledThreadPool(poolSize);
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.execute(new Task());
+		
+		executorService.shutdown();
+	}
+	
+	/**
+	 * 设置计划任务调用的线程池
+	 * 
+	 * @throws InterruptedException 
+	 * 
+	 * @MethodName: testScheduledThreadPoolWithThradFactory 
+	 * @Description: 
+	 * @throws
+	 */
+	@Test
+	public void testScheduledThreadPoolWithThradFactory() throws InterruptedException{
+		// 线程内创建至少poolSize个线程，可以根据业务处理设置处理时间 
+		//ScheduledExecutorService继承自ExecutorService
+		ThreadFactory threadFactory=Executors.defaultThreadFactory();
+		ScheduledExecutorService  executorService=Executors.newScheduledThreadPool(poolSize, threadFactory);
 		executorService.awaitTermination(3000, TimeUnit.MILLISECONDS);
 		//ExecutorService executorService=Executors.newScheduledThreadPool(poolSize);
 		
